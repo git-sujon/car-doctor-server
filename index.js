b7357e6150be3a082ee5c8b012cee21ef3d1c2cd
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-02 12:18:31 +0600

    MongoDb file added

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
+const cors= require('cors')
+require ('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app= express()
 const port = process.env.PORT || 5000
-
 
 // Middleware 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.prabmlk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send("Server is Running")
 })
 
 app.listen(port, ()=>{
-    console.log(`${port} por vvt`)
+    console.log(`${port} port`)
 })
